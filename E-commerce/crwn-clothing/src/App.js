210ed1a40e342a9d04c425efc2e5e80b21a29be7
Carlos Enrique <enrique_lc@ciencias.unam.mx>
2020-06-05 20:45:54 -0500

    signin and signup finished

--- E-commerce/crwn-clothing/src/App.js
+++ E-commerce/crwn-clothing/src/App.js
@@ -4,16 +4,57 @@ import './App.css';
 import  HomePage  from './pages/homepage/homepage.component';
 import ShopPage  from './pages/shop/shop.component';
 import "./pages/homepage/homepage.styles.scss";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage  from "./pages/signin-signup/signin-signup.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils.js";
 
-function App() {
-  return (
-    <div>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/shop" component={ShopPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+      }      
+      
+      this.setState({currentUser: userAuth});
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
