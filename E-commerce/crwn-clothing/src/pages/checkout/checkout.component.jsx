adc24810f836430950e1889d21ff16cc89bca755
Carlos Enrique <enrique_lc@ciencias.unam.mx>
2020-06-08 21:39:42 -0500

    updating stripe integration and updating our favicon.ico

--- E-commerce/crwn-clothing/src/pages/checkout/checkout.component.jsx
+++ E-commerce/crwn-clothing/src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cart.selectors";
 
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
 
 import "./checkout.styles.scss";
 
@@ -35,6 +36,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         <div className="total">
             <span>TOTAL: ${total}</span>
         </div>
+        <StripeCheckoutButton price={total} />
     </div>
 );
 
